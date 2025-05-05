c031ae56637e6e73d34bd7aed6faf856ebabe665
anirudhchakraborty <preetiujjal@gmail.com>
2025-05-05 07:23:09 +0530

    Layout and Button work done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+    implementation("androidx.activity:activity:1.8.0") // Downgraded from 1.10.1
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
